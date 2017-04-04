b10f6aedbd9119bf72735ce2e3cf54074e9dd3e4
Th1eF <phillip_osial@hotmail.com>
2017-04-03 20:24:40 -0400

    Adding data, modified code

--- scripts/statsCanadaAnalysis.r
+++ scripts/statsCanadaAnalysis.r
@@ -1,1 +1,23 @@
-statsCanada <- load("data/CHH_ProvincialHeartHealthData.RData")
+load("data/Heart Survey.RData")
+filterData <- as.data.frame(x)
+filterData <- filterData[, colSums(is.na(filterData)) == 0]
+#filterData[["MSYS"]] = NULL
+#filterData[["AGE"]] = NULL
+
+library(gRim)
+bn.gs <- gs(filterData);
+bn2 <- iamb(filterData);
+bn3 <- fast.iamb(filterData);
+bn4 <- inter.iamb(filterData);
+
+
+bn.hc <- hc(filterData, score = "aic")
+bn.hc
+
+library(bnlearn)
+#highlight.opts <- list(nodes = c("hdap"), fill = "grey")
+graphviz.plot(bn.gs)
+graphviz.plot(bn2)
+graphviz.plot(bn3)
+graphviz.plot(bn4)
+graphviz.plot(bn.hc)
